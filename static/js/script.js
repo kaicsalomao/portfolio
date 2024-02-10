c7e790ea9774ff528a297a000dec15ef9487ec76
Kaic Salomao <ikaicsh@gmail.com>
2024-02-10 12:49:56 -0300

    more improvements

--- static/js/script.js
+++ static/js/script.js
@@ -1,35 +1,71 @@
 particlesJS.load("particles-container", "static/particles.json");
 var scrollOptions = {
   delay: 300,
-  duration: 700,
+  duration: 500,
   distance: "30px",
   origin: "top",
 };
-ScrollReveal().reveal(".about", scrollOptions);
-ScrollReveal().reveal("#about-description", scrollOptions);
+
+ScrollReveal().reveal(".about h2", {
+  delay: 300,
+  duration: 500,
+  distance: "30px",
+  origin: "left"
+});
+ScrollReveal().reveal("#about-description", {
+  delay: 400,
+  duration: 500,
+  distance: "30px",
+  origin: "top"
+});
 ScrollReveal().reveal(".line-ligature", {
   delay: 300,
-  duration: 700,
+  duration: 500,
 });
 ScrollReveal().reveal(".more-information p", {
-  delay: 500,
+  delay: 300,
   origin: "left",
   distance: "40px",
   duration: 700,
 });
 ScrollReveal().reveal(".line-aditional", {
-  delay: 100,
-  duration: 700,
+  delay: 300,
+  duration: 500,
 });
-ScrollReveal().reveal(".projects", scrollOptions);
+ScrollReveal().reveal(".projects h2", {
+  delay: 300,
+  duration: 500,
+  distance: "30px",
+  origin: "left"
+});
+ScrollReveal().reveal(".projects-list", {
+  delay: 400,
+  duration: 500,
+  distance: "30px",
+  origin: "top"
+});
 ScrollReveal().reveal(".contact", scrollOptions);
 ScrollReveal().reveal(".contacts ul", scrollOptions);
 
 // Previnir inicio antecipado
 document.addEventListener("DOMContentLoaded", () => {
-  new TypeIt(".header-description", {
-    strings: "Ajudando empresas e pessoas a contruir aplicativos e sites digitais.",
-    speed: 60,
-    loop: false,
-  }).go();
+  var content = [
+    "Programador",
+    "Designer",
+    "Estudante",
+    "Entusiasta da tecnologia",
+  ];
+  var instance = new TypeIt(".header-description", {
+    speed: 90,
+    startDelay: 500,
+    loop: true,
+  });
+  content.forEach((str, index) => {
+    if (index < content.length - 1) {
+      instance.type(str).pause(1200).delete();
+    } else {
+      instance.type(str).pause(1700);
+    }
+  });
+  instance.go()
 });
